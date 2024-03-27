afd92d724fe38e970fc6c3a14810f5b1b0839c7d
floconno <floconno@asu.edu>
2024-03-27 00:07:27 -0700

    fixed badge issue and cleaned up code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require('inquirer');
 const { writeFile } = require('fs').promises;
 const {generateMarkdown} = require('./utils/generateMarkdown');
@@ -107,23 +107,3 @@ const init = () => {
   init();
 
 
-
-
-
-
-
-
-
-
-
-// // TODO: Create an array of questions for user input
-// const questions = [];
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,12 @@
 
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(answers) {
     if (answers.license !== "NONE") {
-    return `![License](https://img.shields.io/badge/License-${answers.license}.0-blue.svg)
+    return `![GitHub License](https://img.shields.io/github/license/${answers.username}/${answers.title})
       `;
     }
     return "";
     }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(answers) {
     if (answers.license === "NONE") {
       return "";
@@ -19,8 +15,6 @@ function renderLicenseLink(answers) {
     }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(answers) {
     if (answers.license === "NONE") {
       return "";
@@ -30,7 +24,6 @@ function renderLicenseSection(answers) {
     }
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
   return `# ${answers.title}
   ${renderLicenseBadge(answers)}
@@ -68,7 +61,6 @@ function generateMarkdown(answers) {
 
 `;
 }
-
 
 
 
